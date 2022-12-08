efbecf85d6f6b4c02ac1aefe396848987f18591e
BogdanDraghia <bgd.draghia@gmail.com>
2022-12-08 23:32:42 +0000

    added lambda policy on Helpers infra stack, modified lamdba handler: added describe testing cluster, reverse git ignore index.js

--- lib/helpers-infra-stack.ts
+++ lib/helpers-infra-stack.ts
@@ -1,18 +1,23 @@
 import cdk = require('aws-cdk-lib');
 import lambda = require("aws-cdk-lib/aws-lambda");
 import aws_lambda_nodejs = require("aws-cdk-lib/aws-lambda-nodejs");
-
+import { PolicyStatement } from "aws-cdk-lib/aws-iam"
 import apigateway = require('aws-cdk-lib/aws-apigatewayv2')
 import path =require('path')
 export class HelpersInfraStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const lambdaPolicy = new PolicyStatement()
+        lambdaPolicy.addActions("ecs:*")
+        lambdaPolicy.addResources("arn:aws:ecs:eu-west-3:921323820524:cluster/fargate-spot")
         const lambdaFunction = new lambda.Function(this,'ecs-helper-portofolio',
         {
             memorySize: 1024,
             runtime: lambda.Runtime.NODEJS_16_X,
             code: lambda.Code.fromAsset(path.join(__dirname, "/../src/lambda")),
             handler: 'index.handler',
+            initialPolicy:[lambdaPolicy]
         })
         const lambdaURL = new lambda.CfnUrl(this,"url-lambda",{
             targetFunctionArn:lambdaFunction.functionArn,
